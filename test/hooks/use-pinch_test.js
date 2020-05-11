648c091a20159699a459170c43c44d401675227d
Patrik Kullman <patrik.kullman@neovici.se>
2020-05-12 01:26:33 +0200

    fix: pinch state without useReducer
    After Slack discussions around reducers I wanted to prototype another, less complex approach to the problem.
    
    Switching from useReducer to a wrapped useState made the code a lot easier to read and maintain in my view.
    
    1. There is no need for a reducing "dispatch" function or an "action" event with a loose contract. Each "action" can be a method of its own, with clear parameters/arguments and even JSDoc for better DevUX and obvious usage. The DevUX is also of course improved by things like intellisense for the methods and arguments.
    2. #1 makes it easier to both read the core "reducer" and understand what it does and each method's parameters/context and output, but also the using layer since the methods/actions are more clear.
    3. Going from a nested switch case to explicit status transition conditions in the methods makes the behavior a lot more obvious and the transitions (or preventing them) becomes a lot more expressive and clear.
    4. Going from the reducer approach of "appending" to previous state to rather set a complete state (based on previous state or not) makes it easier to evaluate the outcome of an action in isolation since we don't have to care about the combination of actions/sub-states anymore.
    5. The user doesn't really need anything else than the clean hook, so there's no need to export things like initState or status consts like PINCHING anymore.
    
    Also, this pattern doesn't at this point provide a "stable" function dependency like "dispatch" so dropping the useCallback() pattern.
    
    Signed-off-by: Patrik Kullman <patrik.kullman@neovici.se>

--- test/hooks/use-pinch_test.js
+++ test/hooks/use-pinch_test.js
@@ -14,15 +14,15 @@ describe('use-pinch', () => {
 		const tag = 'use-pinch',
 
 			App = () => {
-				const [status, delta, start] = usePinch();
-				return html`<span @touchstart=${start}>${status}, ${delta}</span>`;
+				const [isPinching, delta, start] = usePinch();
+				return html`<span @touchstart=${start}>${isPinching}, ${delta}</span>`;
 			};
 
 		customElements.define(tag, component(App));
 
 		const teardown = attach(tag);
 		await cycle();
-		expect(text()).to.equal('init, 0', 'no touching');
+		expect(text()).to.equal('false, 0', 'no touching');
 
 		const target = host.firstChild.shadowRoot.firstElementChild;
 
@@ -31,7 +31,7 @@ describe('use-pinch', () => {
 			y: 10
 		}], target);
 		await cycle();
-		expect(text()).to.equal('init, 0', 'touch with one finger');
+		expect(text()).to.equal('false, 0', 'touch with one finger');
 
 		makeMultiTouchEvent('touchstart', [{
 			x: 10,
@@ -41,7 +41,7 @@ describe('use-pinch', () => {
 			y: 50
 		}], target);
 		await cycle();
-		expect(text()).to.equal('pinching, 0', 'touch with second finger');
+		expect(text()).to.equal('true, 0', 'touch with second finger');
 
 		makeMultiTouchEvent('touchmove', [{
 			x: 10,
@@ -51,21 +51,21 @@ describe('use-pinch', () => {
 			y: 100
 		}], document);
 		await cycle();
-		expect(text()).to.equal('pinching, 70.71067811865476', 'move second finger');
+		expect(text()).to.equal('true, 70.71067811865476', 'move second finger');
 
 		makeMultiTouchEvent('touchend', [{
 			x: 10,
 			y: 10
 		}], document);
 		await cycle();
-		expect(text()).to.equal('init, 0', 'raise one finger');
+		expect(text()).to.equal('false, 0', 'raise one finger');
 
 		makeMultiTouchEvent('touchmove', [{
 			x: 20,
 			y: 20
 		}], document);
 		await cycle();
-		expect(text()).to.equal('init, 0', 'move the finger');
+		expect(text()).to.equal('false, 0', 'move the finger');
 
 		makeMultiTouchEvent('touchstart', [{
 			x: 20,
@@ -75,7 +75,7 @@ describe('use-pinch', () => {
 			y: 30
 		}], target);
 		await cycle();
-		expect(text()).to.equal('pinching, 0', 'touch with second finger');
+		expect(text()).to.equal('true, 0', 'touch with second finger');
 
 		makeMultiTouchEvent('touchmove', [{
 			x: 20,
@@ -85,18 +85,18 @@ describe('use-pinch', () => {
 			y: 30
 		}], document);
 		await cycle();
-		expect(text()).to.equal('pinching, 10', 'move first finger');
+		expect(text()).to.equal('true, 10', 'move first finger');
 
 		makeMultiTouchEvent('touchend', [{
 			x: 20,
 			y: 30
 		}], document);
 		await cycle();
-		expect(text()).to.equal('init, 0', 'raise first finger');
+		expect(text()).to.equal('false, 0', 'raise first finger');
 
 		makeMultiTouchEvent('touchend', [], document);
 		await cycle();
-		expect(text()).to.equal('init, 0', 'raise second finger');
+		expect(text()).to.equal('false, 0', 'raise second finger');
 
 		teardown();
 	});
