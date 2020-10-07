20be6e5f09e1736e659162760028999fdf161f03
Meghea Iulian <iulian.meghea@gmail.com>
2020-10-07 10:44:07 +0300

    chore(deps): update

--- shell.nix
+++ shell.nix
@@ -3,14 +3,22 @@
 with import
   (
     builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/0a14605.tar.gz";
-      sha256 = "154ypjfhy9qqa0ww6xi7d8280h85kffqaqf6b6idymizga9ckjcd";
+      url = "https://github.com/NixOS/nixpkgs/archive/daaa0e33505082716beb52efefe3064f0332b521.tar.gz";
+      sha256 = "15vprzpbllp9hy5md36ch1llzhxhd44d291kawcslgrzibw51f95";
     }
-  ) { };
+  )
+{ };
 mkShell {
-  buildInputs = [ nodejs-12_x python3 firefox ];
+  buildInputs = [ nodejs-12_x python3 firefox jre azure-storage-azcopy google-chrome ];
   shellHook = ''
     export CHROME_BIN=${google-chrome}/bin/google-chrome-stable
     export PATH=$(npm bin):$PATH
+
+    link_selenium () {
+      if [ -d "./node_modules" ]; then
+        ln -sfnv "${chromedriver}/bin/chromedriver" "$(find "./node_modules" -path '**/chromedriver/*-chromedriver')"
+        ln -sfnv "${geckodriver}/bin/geckodriver" "$(find "./node_modules" -path '**/geckodriver/*-geckodriver')"
+      fi
+    }
   '';
 }
