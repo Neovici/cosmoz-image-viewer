998b2c92ed84023f4b6e4bb09655414ff55480bf
Cristian Necula <virusz@gmail.com>
2022-04-20 12:09:12 +0300

    ci: use standard neovici test runner config

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,31 +1,3 @@
-import { chromeLauncher } from '@web/test-runner';
-import { seleniumLauncher } from '@web/test-runner-selenium';
-import webdriver from 'selenium-webdriver';
-import firefox from 'selenium-webdriver/firefox.js';
+import cfg from '@neovici/cfg/web/test-runner.mjs';
 
-export default {
-	nodeResolve: true,
-	coverageConfig: {
-		reportDir: 'coverage',
-		threshold: {
-			statements: 70,
-			branches: 70,
-			functions: 50,
-			lines: 70
-		}
-	},
-	testFramework: {
-		config: {
-			ui: 'tdd',
-			timeout: 20000
-		}
-	},
-	files: ['**!(node_modules)/*.test.js'],
-	browsers: [
-		chromeLauncher(),
-		seleniumLauncher({
-			driverBuilder: new webdriver.Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().headless())
-		})
-	]
-
-};
+export default cfg;
