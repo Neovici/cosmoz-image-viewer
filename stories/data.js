0fd7f4860b0fc8c3252de22ca2648ddb1bfdec77
Cristian Necula <virusz@gmail.com>
2023-12-11 03:29:19 +0200

    fix: handle lazy images in download and print pdf

--- stories/data.js
+++ stories/data.js
@@ -1,6 +1,9 @@
 export const images = [
 	'stories/images/stockholm.jpg',
 	'this-is-a-loading-error.jpg',
-	'stories/images/a_size.png',
-	'stories/images/strasbourg.jpg',
+	() => 'stories/images/a_size.png',
+	() =>
+		new Promise((resolve) =>
+			setTimeout(() => resolve('stories/images/strasbourg.jpg'), 500),
+		),
 ];
