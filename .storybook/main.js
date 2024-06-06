9804e7a0bda264aee30f724bc5f559e5ae1f01f3
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2024-06-06 10:29:06 +0300

    feat: make storybook 7 work

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,23 @@
-module.exports = {
-  stories: ['../stories/**/*.stories.{js,mdx}'],
+import esbuild from 'rollup-plugin-esbuild';
+
+/** @type { import('@web/storybook-framework-web-components').StorybookConfig } */
+const config = {
+	stories: ['../stories/**/*.stories.{js,ts,mdx}'],
+	addons: ['@storybook/addon-essentials', '@storybook/addon-links'],
+	framework: {
+		name: '@web/storybook-framework-web-components',
+	},
+	docs: {
+		autodocs: true,
+	},
+	/* Try to make the build parse TS files */
+	async rollupFinal(config) {
+		// add extra configuration for rollup
+		// e.g. a new plugin
+		config.plugins.push(esbuild({}));
+
+		return config;
+	},
 };
+
+export default config;
