f10afedaedf76652b38ebe64c49e689ac41ccc8d
basilgood <basilgood@users.noreply.github.com>
2024-07-29 08:38:42 +0000

    Remove preview for PR 188 🛬

--- pr/pr-188/entry-preview-BaboETd1.js
+++
@@ -1,7 +0,0 @@
-export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-C8bcENUK.js';
-import './index-Kjm4kNkQ.js';
-import './directive-helpers-BXW8nDWk.js';
-
-var parameters={renderer:"web-components"};
-
-export { parameters };
