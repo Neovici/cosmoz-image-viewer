7711170deacbd2113354eeca27b2dbaf5a19f8da
basilgood <elsile69@yahoo.com>
2024-08-16 07:47:06 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-image-viewer@5a466361fd9e2cfdcc1efa4f935438f49d653ad4 🚀

---
+++ haunted-pan-zoom.stories-wWnTRryZ.js
@@ -0,0 +1,27 @@
+import { x } from './directive-helpers-mVbraAne.js';
+import './cosmoz-image-viewer-Is1tbblg.js';
+
+var hauntedPanZoom_stories = {
+  title: "Pan Zoom",
+  component: "haunted-pan-zoom"
+};
+const Template = ({ disabled, height, zoomStiffness, panStiffness }) => x`
+	<haunted-pan-zoom
+		src="a_size.png"
+		style="height: ${height}px;"
+		?disabled=${disabled}
+		.zoomStiffness=${zoomStiffness}
+		.panStiffness=${panStiffness}
+	>
+	</haunted-pan-zoom>
+`;
+const PanZoom = Template.bind({});
+PanZoom.args = {
+  disabled: false,
+  height: 400,
+  zoomStiffness: 0.3,
+  panStiffness: 1
+};
+const __namedExportsOrder = ["PanZoom"];
+
+export { PanZoom, __namedExportsOrder, hauntedPanZoom_stories as default };
---
+++ inline-module-a412f23235eb971c4a27c3950e98fd49.js
@@ -0,0 +1,38 @@
+import './sb-preview/runtime.js';
+
+const channel = __STORYBOOK_MODULE_CHANNEL_POSTMESSAGE__.createChannel({ page: "preview" });
+__STORYBOOK_MODULE_PREVIEW_API__.addons.setChannel(channel);
+window.__STORYBOOK_ADDONS_CHANNEL__ = channel;
+const { SERVER_CHANNEL_URL } = globalThis;
+if (SERVER_CHANNEL_URL) {
+  const serverChannel = __STORYBOOK_MODULE_CHANNEL_WEBSOCKET__.createChannel({ url: SERVER_CHANNEL_URL });
+  __STORYBOOK_MODULE_PREVIEW_API__.addons.setServerChannel(serverChannel);
+  window.__STORYBOOK_SERVER_CHANNEL__ = serverChannel;
+}
+
+const importers = {
+  "./stories/cosmoz-image-viewer.stories.js": () => import('./cosmoz-image-viewer.stories-7krUH0Nh.js'),
+  "./stories/haunted-pan-zoom.stories.js": () => import('./haunted-pan-zoom.stories-wWnTRryZ.js')
+};
+function importFn(path) {
+  return importers[path]();
+}
+
+const getProjectAnnotations = async () => {
+  const configs = await Promise.all([
+    import('./entry-preview-UHbUvXwy.js'),
+    import('./entry-preview-docs-hBTbfxOx.js'),
+    import('./preview-Z882EUFE.js'),
+    import('./preview-LSB5aSA_.js'),
+    import('./preview-fr89Ma8P.js'),
+    import('./preview-MTg22G6e.js'),
+    import('./preview-nPmPU166.js'),
+    import('./preview-GQUB9WQH.js'),
+    import('./preview-I57bIBGH.js')
+  ]);
+  return __STORYBOOK_MODULE_PREVIEW_API__.composeConfigs(configs);
+};
+window.__STORYBOOK_PREVIEW__ = window.__STORYBOOK_PREVIEW__ || new __STORYBOOK_MODULE_PREVIEW_API__.PreviewWeb();
+window.__STORYBOOK_STORY_STORE__ = window.__STORYBOOK_STORY_STORE__ || window.__STORYBOOK_PREVIEW__.storyStore;
+window.__STORYBOOK_CLIENT_API__ = window.__STORYBOOK_CLIENT_API__ || new __STORYBOOK_MODULE_PREVIEW_API__.ClientApi({ storyStore: window.__STORYBOOK_PREVIEW__.storyStore });
+window.__STORYBOOK_PREVIEW__.initialize({ importFn, getProjectAnnotations });
