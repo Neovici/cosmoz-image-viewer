7b48263119bdab758c9dee6006be5ae0a6b391e8
basilgood <basilgood@users.noreply.github.com>
2025-07-21 21:45:43 +0000

    Remove preview for PR 193 🛬

--- pr/pr-193/preview-Z882EUFE.js
+++
@@ -1,3 +0,0 @@
-var parameters={docs:{renderer:async()=>{let{DocsRenderer}=await import('./WithTooltip-V3YHNWJZ-qAkN2K7f.js').then(function (n) { return n.D; });return new DocsRenderer}}};
-
-export { parameters };
