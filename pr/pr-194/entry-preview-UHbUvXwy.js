cc637d5600fc4b295a58d173e4585b57eb94fdaf
basilgood <basilgood@users.noreply.github.com>
2025-08-04 07:26:55 +0000

    Remove preview for PR 194 🛬

--- pr/pr-194/entry-preview-UHbUvXwy.js
+++
@@ -1,7 +0,0 @@
-export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-843PFx6h.js';
-import './index-PiJ8i7Ux.js';
-import './directive-helpers-mVbraAne.js';
-
-var parameters={renderer:"web-components"};
-
-export { parameters };
