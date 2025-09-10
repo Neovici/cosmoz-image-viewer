d256d4ab73e54a94e4daa8dc7e1f9ec5ab3ba208
basilgood <elsile69@yahoo.com>
2025-09-10 11:51:11 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-image-viewer@b7ff61c732954a3ef8f5ebcdd9f85ee86eac6e11 🚀

--- entry-preview-CkMu6tQy.js
+++
@@ -1,7 +0,0 @@
-export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-CWmS9P6l.js';
-import './index-Kjm4kNkQ.js';
-import './directive-helpers-DTirirN5.js';
-
-var parameters={renderer:"web-components"};
-
-export { parameters };
