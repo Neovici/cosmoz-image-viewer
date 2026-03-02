c2d801e655951d3457b58a6278db5faccf4d6882
cristinecula <VirusZ@gmail.com>
2026-03-02 22:51:37 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-image-viewer@5e687d00b44ce23c2b42e3212100a6181ebb12f0 🚀

--- assets/haunted-pan-zoom.stories-9X_p6VUc.js
+++ assets/haunted-pan-zoom.stories-9X_p6VUc.js
@@ -1,4 +1,4 @@
-import{x as t}from"./iframe-zk_1k1JR.js";import"./cosmoz-image-viewer-BscedSsX.js";const d={title:"Pan Zoom",component:"haunted-pan-zoom"},i=({disabled:n,height:e,zoomStiffness:o,panStiffness:a})=>t`
+import{x as t}from"./iframe-D0zZY896.js";import"./cosmoz-image-viewer-CYEjxmpH.js";const d={title:"Pan Zoom",component:"haunted-pan-zoom"},i=({disabled:n,height:e,zoomStiffness:o,panStiffness:a})=>t`
     <haunted-pan-zoom
         src="stories/images/a_size.png"
         style="${e};"
