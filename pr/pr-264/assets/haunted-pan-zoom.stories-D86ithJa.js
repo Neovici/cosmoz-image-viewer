3906791fa155dd9998692313f1267d1ade501438
cristinecula <cristinecula@users.noreply.github.com>
2026-03-30 08:57:41 +0000

    Deploy preview for PR 264 🛫

--- pr/pr-264/assets/haunted-pan-zoom.stories-D86ithJa.js
+++ pr/pr-264/assets/haunted-pan-zoom.stories-D86ithJa.js
@@ -1,4 +1,4 @@
-import{b as t}from"./iframe-CfLoxjgR.js";import"./cosmoz-image-viewer-C-1POiFl.js";import"./preload-helper-PPVm8Dsz.js";const f={title:"Pan Zoom",component:"haunted-pan-zoom"},i=({disabled:n,height:e,zoomStiffness:o,panStiffness:a})=>t`
+import{b as t}from"./iframe-DbZNKACy.js";import"./cosmoz-image-viewer-DkYi-FD7.js";import"./preload-helper-PPVm8Dsz.js";const f={title:"Pan Zoom",component:"haunted-pan-zoom"},i=({disabled:n,height:e,zoomStiffness:o,panStiffness:a})=>t`
     <haunted-pan-zoom
         src="stories/images/a_size.png"
         style="${e};"
