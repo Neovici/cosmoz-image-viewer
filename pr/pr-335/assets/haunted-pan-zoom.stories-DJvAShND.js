f6040df3688b3bc85b8e15bacda76d233cc3b10b
cristinecula <cristinecula@users.noreply.github.com>
2026-07-03 06:42:02 +0000

    Deploy preview for PR 335 🛫

--- pr/pr-335/assets/haunted-pan-zoom.stories-DJvAShND.js
+++ pr/pr-335/assets/haunted-pan-zoom.stories-DJvAShND.js
@@ -1,4 +1,4 @@
-import{b as a}from"./iframe-WJ8-Z_xu.js";import"./cosmoz-image-viewer-mhmGT4QR.js";import"./preload-helper-PPVm8Dsz.js";const p={title:"Pan Zoom",component:"haunted-pan-zoom"},i=({disabled:s,height:n,zoomStiffness:t})=>a`
+import{b as a}from"./iframe-DMpcS_uw.js";import"./cosmoz-image-viewer-C3gLCmht.js";import"./preload-helper-PPVm8Dsz.js";const p={title:"Pan Zoom",component:"haunted-pan-zoom"},i=({disabled:s,height:n,zoomStiffness:t})=>a`
     <haunted-pan-zoom
         src="stories/images/a_size.png"
         style="${n};"
